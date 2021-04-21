06b5caf1c3a5585879514b6fb523b84d8c81c888
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-04-21 11:21:34 +0100

    Add french translation

--- hello.R
+++ hello.R
@@ -1,6 +1,8 @@
 say_hello <- function(language) {
   if (language == "english") {
     message("hello")
+  } else if (language == "french") {
+    message("bonjour")
   } else {
     stop("I can't say hello in ", language)
   }
