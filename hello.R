8dcf7e24e15b960e49c9f5f48ed46540659ec9ca
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-04-21 12:08:35 +0100

    Add spanish translation

--- hello.R
+++ hello.R
@@ -3,6 +3,8 @@ say_hello <- function(language) {
     message("hello")
   } else if (language == "french") {
     message("bonjour")
+  } else if (language == "spanish") {
+    message("hola")
   } else {
     stop("I can't say hello in ", language)
   }
